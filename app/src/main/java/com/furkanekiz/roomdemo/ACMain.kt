d5cb5f46da7de1c0520360003bd523613da0dda2
furkanekiz <furkanekiz.06@gmail.com>
2022-08-16 17:33:20 +0300

    entity added

---
+++ app/src/main/java/com/furkanekiz/roomdemo/ACMain.kt
@@ -0,0 +1,11 @@
+package com.furkanekiz.roomdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ACMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ac_main)
+    }
+}
