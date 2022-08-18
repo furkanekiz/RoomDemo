f468fa2263f2d0a6377605e4cd63279cb9212d8d
furkanekiz <furkanekiz.06@gmail.com>
2022-08-18 12:04:08 +0300

    list_item.xml, adapter, item click, delete & update button added

--- app/src/main/java/com/furkanekiz/roomdemo/ACMain.kt
+++ app/src/main/java/com/furkanekiz/roomdemo/ACMain.kt
@@ -3,9 +3,12 @@ package com.furkanekiz.roomdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.furkanekiz.roomdemo.databinding.AcMainBinding
+import com.furkanekiz.roomdemo.db.Subscriber
 import com.furkanekiz.roomdemo.db.SubscriberDatabase
 import com.furkanekiz.roomdemo.db.SubscriberRepository
 
@@ -17,26 +20,41 @@ class ACMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.ac_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.ac_main)
 
         val dao = SubscriberDatabase.getInstance(application).subscriberDAO
         val repository = SubscriberRepository(dao)
         val factory = SubscriberViewModelFactory(repository)
 
-        subscriberViewModel = ViewModelProvider(this,factory)[SubscriberViewModel::class.java]
+        subscriberViewModel = ViewModelProvider(this, factory)[SubscriberViewModel::class.java]
 
         binding.myViewModel = subscriberViewModel
 
         binding.lifecycleOwner = this
 
+        initRecyclerView()
+
+    }
+
+    private fun initRecyclerView() {
+        binding.rvSubscriber.layoutManager = LinearLayoutManager(this)
         displaySubscribersList()
 
     }
 
-    private fun displaySubscribersList(){
+    private fun displaySubscribersList() {
         subscriberViewModel.getSaveSubscribers().observe(this) {
-            Log.i("MyTag",it.toString())
+            Log.i("MyTag", it.toString())
+            binding.rvSubscriber.adapter = MyRecyclerViewAdapter(it) { selectedItem: Subscriber ->
+                listItemClicked(
+                    selectedItem
+                )
+            }
         }
+    }
 
+    private fun listItemClicked(subscriber: Subscriber) {
+        Toast.makeText(this, "Selected name is ${subscriber.name}", Toast.LENGTH_LONG).show()
+        subscriberViewModel.initUpdateAndDelete(subscriber)
     }
 }
---
+++ app/src/main/java/com/furkanekiz/roomdemo/MyRecyclerViewAdapter.kt
@@ -0,0 +1,34 @@
+package com.furkanekiz.roomdemo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.furkanekiz.roomdemo.databinding.ListItemBinding
+import com.furkanekiz.roomdemo.db.Subscriber
+
+class MyRecyclerViewAdapter(private val subscriberList: List<Subscriber>, private val clickListener :(Subscriber)->Unit): RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ListItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(subscriberList[position], clickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return subscriberList.size
+    }
+
+}
+
+class MyViewHolder(private val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(subscriber: Subscriber, clickListener :(Subscriber)->Unit) {
+        binding.tvName.text = subscriber.name
+        binding.tvEmail.text = subscriber.email
+        binding.listItemLayout.setOnClickListener {
+            clickListener(subscriber)
+        }
+
+    }
+}
